5f7781ad771b03432c53e86a6f2ff566b8645a98
ncernek <ncernek@gmail.com>
2018-12-09 23:53:25 -0700

    rename parser file and simplify

---
+++ app/parsers.py
@@ -0,0 +1,26 @@
+ANY = '*'
+
+MULTIPLE_CHOICE = {
+    'a': ['a', 'a)'],
+    'b': ['b', 'b)'],
+    'c': ['c', 'c)'],
+    'd': ['d', 'd)'],
+}
+
+YES_NO = {
+    'yes': ['yes', 'y', 'ye', 'ya', 'yea', 'yep', 'yup', 'yeah'],
+    'no': ['no', 'n', 'na', 'nope'],
+}
+
+
+def parse(inbound, inbound_format):
+    '''combine all parsers'''
+    if inbound_format == ANY:
+        return inbound
+    else:
+        inbound = inbound.lower()
+        for term, matches in inbound_format.items():
+            if inbound in matches:
+                return term
+    # returning None signals to the app that parsing the inbound failed
+    return None
