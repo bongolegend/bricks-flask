a6394030cf747a54c2ea3ea1d53a57c348dc119f
ncernek <ncernek@gmail.com>
2018-12-18 15:08:42 -0700

    add testing credentials to config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import settings
 class Config:
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TWILIO_ACCOUNT_SID = os.environ.get('TEST_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TEST_AUTH_TOKEN')
+    TWILIO_PHONE_NUMBER = os.environ.get('TEST_PHONE_NUMBER')
 
 
 class DevConfig(Config):
@@ -17,9 +20,13 @@ class TestConfig(Config):
     TESTING = True
 
 
+
 class ProdConfig(Config):
     db_user = os.environ.get('CLOUD_SQL_USERNAME')
     db_password = os.environ.get('CLOUD_SQL_PASSWORD')
     db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
     HOST = f'/cloudsql/{os.environ.get("CLOUD_SQL_CONNECTION_NAME")}'
     SQLALCHEMY_DATABASE_URI = f'postgres:///{db_name}?host={HOST}&user={db_user}&password={db_password}&sslmode=disable'
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
