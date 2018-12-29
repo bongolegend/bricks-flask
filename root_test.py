011962844342373acb779339b02728efdbd396b1
ncernek <ncernek@gmail.com>
2018-12-29 13:55:01 -0600

    fix test auto discovery

--- root_test.py
+++ root_test.py
@@ -2,45 +2,15 @@
 import unittest
 from app.models import Exchange
 from tests.test_all_routers import TestAllRouters
-from tests.test_notify import TestNotifications
 from tests.test_invitation import TestInitOnboardingInvited
-from tests.test_chat import TestChat
-from tests.actions.test_multiplayer import TestMultiplayer
-from tests.actions.test_settings import TestSettings
-
-
-def generator(router, inbound=str()):
-    '''function that returns one test function per router'''
-    def test(self):
-        print(f"""
-=============================
-TESTING ROUTER: {router.__name__}
-TESTING INBOUND: {inbound}
-=============================
-        """)
-        exchange = Exchange(router=router.__name__, user=self.mitch)
-        self.db.session.add(exchange)
-        response = self.client.post('/chat', data=dict(Body=inbound, From=self.mitch.phone_number))
-
-    return test
-
-
-def generate_tests_for_class(test_class):
-    '''For a given test class, generate all of its tests'''
-    for name, router in test_class.get_routers().items():
-        if isinstance(router.inbound_format, dict):
-            for inbound in router.inbound_format:
-                test_name = f'test_{name}_{inbound}'
-                test = generator(router,  inbound)
-                setattr(test_class, test_name, test)
-        else:
-            test_name = f"test_{name}"
-            test = generator(router)
-            setattr(test_class, test_name, test)
-
+from tests.tools import generate_tests_for_class
 
 if __name__ == '__main__':
     generate_tests_for_class(TestAllRouters)
     generate_tests_for_class(TestInitOnboardingInvited)
 
-    unittest.main(warnings='ignore')    
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+
+    runner = unittest.TextTestRunner(warnings='ignore')
+    runner.run(tests)
--- tests/tools.py
+++ tests/tools.py
@@ -1,5 +1,6 @@
 from app.base_init import init_app, init_db
-
+import unittest
+from app.models import Exchange
 
 def with_app_context(func):
     '''inits a new app and db, and runs func within it. used for functions run by scheduler. useful for tests tho'''
@@ -11,3 +12,33 @@ def with_app_context(func):
         with app.app_context():
             return func(*args, **kwargs)
     return wrapper
+
+
+def generator(router, inbound=str()):
+    '''function that returns one test function per router'''
+    def test(self):
+        print(f"""
+=============================
+TESTING ROUTER: {router.__name__}
+TESTING INBOUND: {inbound}
+=============================
+        """)
+        exchange = Exchange(router=router.__name__, user=self.mitch)
+        self.db.session.add(exchange)
+        response = self.client.post('/chat', data=dict(Body=inbound, From=self.mitch.phone_number))
+
+    return test
+
+
+def generate_tests_for_class(test_class):
+    '''For a given test class, generate all of its tests'''
+    for name, router in test_class.get_routers().items():
+        if isinstance(router.inbound_format, dict):
+            for inbound in router.inbound_format:
+                test_name = f'test_{name}_{inbound}'
+                test = generator(router,  inbound)
+                setattr(test_class, test_name, test)
+        else:
+            test_name = f"test_{name}"
+            test = generator(router)
+            setattr(test_class, test_name, test)
