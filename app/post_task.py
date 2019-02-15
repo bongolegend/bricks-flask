d9b6c04447f4193052e86db013f166e2582a13d8
ncernek <ncernek@gmail.com>
2019-02-14 22:03:16 -0700

    insert task to db

--- app/post_task.py
+++ app/post_task.py
@@ -1,27 +1,31 @@
 from flask import jsonify, request, make_response
-from app.models import AppUser
-
+import dateutil.parser
+from app.models import AppUser, Task
+from app import db
 
 def main(user):
-    print("user received is: ", user)
 
     data = request.get_json()
 
-    if "today_task" not in data:
-        message = "today_task not in body"
+    if "today_task" not in data or "due_date" not in data:
+        message = "today_task or due_date not in body"
         print(message)
         json = jsonify({"error_code": "MISSING_KEY", "message": message})
         return make_response(json, 400)
 
-    task = data["today_task"]
-    print("TASK POSTED is: ", task)
+    description = data["today_task"]
+    due_date = dateutil.parser.parse(data["due_date"])
 
-    # make sure the fields make sense
-
-    # insert task into db
-
-    # return success
+    task = Task(
+        description=description,
+        due_date=due_date,
+        active=True,
+        user=user)
+    
+    db.session.add(task)
+    db.session.commit()
+    
     message = "today_task successfully inserted"
     print(message)
-    json = jsonify({"message": message, "task": task})
+    json = jsonify({"message": message, "task": description})
     return make_response(json, 200)
