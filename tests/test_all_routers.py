983808c4983425f1fda62a46753dee89f5594437
Nico Cernek <ncernek@gmail.com>
2019-08-08 11:47:40 -0600

    format with black

--- tests/test_all_routers.py
+++ tests/test_all_routers.py
@@ -1,22 +1,21 @@
 """Module to test routers"""
 import datetime as dt
 import os
-import unittest
+
+from app.constants import US_TIMEZONES, RouterNames, Statuses
+from app.get_router import get_router
+from app.models import AppUser, Exchange, Notification, Point, Task, Team, TeamMember
 from tests.base_test_case import BaseTestCase
 from tests.constants import TEST_TEAM_ID
-from app.models import AppUser, Exchange, Task, Team, TeamMember, Notification, Point
-from app import models
-from app.constants import US_TIMEZONES, Statuses, RouterNames
-from app import parsers
-from app.get_router import get_router
 
 
 class TestAllRouters(BaseTestCase):
-    inviter_number = os.environ.get('TEST_FROM_NUMBER')
+    inviter_number = os.environ.get("TEST_FROM_NUMBER")
 
     @classmethod
     def get_routers(self):
         routers = get_router()
+        # don't test this router
         routers.pop(RouterNames.INIT_ONBOARDING_INVITED)
         return routers
 
@@ -24,69 +23,67 @@ class TestAllRouters(BaseTestCase):
         super().setUp()
         # add a user
         self.mitch = AppUser(
-            phone_number=self.inviter_number, 
-            username='mitch',
-            timezone=US_TIMEZONES['b'])
+            phone_number=self.inviter_number,
+            username="mitch",
+            timezone=US_TIMEZONES["b"],
+        )
 
         self.db.session.add(self.mitch)
         # self.db.session.add(self.blair)
 
         # add a notif
         self.notif = Notification(
-            router = RouterNames.DID_YOU_DO_IT,
-            hour = 21,
-            minute = 0,
-            active = True,
-            user = self.mitch)
-        
+            router=RouterNames.DID_YOU_DO_IT,
+            hour=21,
+            minute=0,
+            active=True,
+            user=self.mitch,
+        )
+
         self.db.session.add(self.notif)
 
         # add exchange
         self.exchange = Exchange(
-            router = RouterNames.DID_YOU_DO_IT,
-            outbound = 'Did you do it?',
-            user = self.mitch,
-            created = dt.datetime.now() - dt.timedelta(days=10))
+            router=RouterNames.DID_YOU_DO_IT,
+            outbound="Did you do it?",
+            user=self.mitch,
+            created=dt.datetime.now() - dt.timedelta(days=10),
+        )
 
         self.db.session.add(self.exchange)
 
         # add point
-        self.point = Point(
-            value = 10,
-            user = self.mitch)    
+        self.point = Point(value=10, user=self.mitch)
 
         # add task
         self.task = Task(
-            description='Win today',
-            due_date = dt.datetime.now(),
-            active = True,
-            exchange = self.exchange,
-            user = self.mitch)
-        
+            description="Win today",
+            due_date=dt.datetime.now(),
+            active=True,
+            exchange=self.exchange,
+            user=self.mitch,
+        )
+
         self.db.session.add(self.task)
 
-        self.inteam = Team(
-            id = TEST_TEAM_ID,
-            founder = self.mitch,
-            name = 'inteam')
+        self.inteam = Team(id=TEST_TEAM_ID, founder=self.mitch, name="inteam")
         self.db.session.add(self.inteam)
 
-        self.pendingteam = Team(
-            founder = self.mitch,
-            name = 'pendingteam')
+        self.pendingteam = Team(founder=self.mitch, name="pendingteam")
         self.db.session.add(self.pendingteam)
 
         self.mitch_member = TeamMember(
-            user = self.mitch,
-            team = self.inteam,
-            inviter = self.mitch,
-            status = Statuses.ACTIVE)
+            user=self.mitch,
+            team=self.inteam,
+            inviter=self.mitch,
+            status=Statuses.ACTIVE,
+        )
         self.db.session.add(self.mitch_member)
 
         self.mitch_member2 = TeamMember(
-            user = self.mitch,
-            team = self.pendingteam,
-            inviter = self.mitch,
-            status = Statuses.PENDING)
+            user=self.mitch,
+            team=self.pendingteam,
+            inviter=self.mitch,
+            status=Statuses.PENDING,
+        )
         self.db.session.add(self.mitch_member2)
-    
--- tests/tools.py
+++ tests/tools.py
@@ -1,43 +1,24 @@
-import functools
-from app.base_init import init_app, init_db
-import unittest
 from app.models import Exchange
-
-def with_app_context(func):
-    '''inits a new app and db, and runs func within it. used for functions run by scheduler. useful for tests tho'''
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        app = init_app()
-        db = init_db(app)
-        kwargs['db'] = db
-        with app.app_context():
-            return func(*args, **kwargs)
-    return wrapper
 
 
 def generator(router, inbound=str()):
-    '''function that returns one test function per router'''
+    """function that returns one test function per router"""
+
     def test(self):
-        print(f"""
-=============================
-TESTING ROUTER: {router.__name__}
-TESTING INBOUND: {inbound}
-=============================
-        """)
         exchange = Exchange(router=router.__name__, user=self.mitch)
         self.db.session.add(exchange)
-        response = self.client.post('/chat', data=dict(Body=inbound, From=self.mitch.phone_number))
+        self.client.post("/chat", data=dict(Body=inbound, From=self.mitch.phone_number))
 
     return test
 
 
 def generate_tests_for_class(test_class):
-    '''For a given test class, generate all of its tests'''
+    """For a given test class, generate all of its tests"""
     for name, router in test_class.get_routers().items():
         if isinstance(router.inbound_format, (dict, tuple)):
             for inbound in router.inbound_format:
-                test_name = f'test_{name}_{inbound}'
-                test = generator(router,  inbound)
+                test_name = f"test_{name}_{inbound}"
+                test = generator(router, inbound)
                 setattr(test_class, test_name, test)
         else:
             test_name = f"test_{name}"
